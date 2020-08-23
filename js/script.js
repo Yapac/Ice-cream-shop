d9a6ba0b4a1415c22344dfbbb597c8d05b3c8cc8
Yassine El Atlassi <55562792+Yapac@users.noreply.github.com>
2020-08-23 14:45:17 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-
+var request_countries= new XMLHttpRequest();
+request_countries.onreadystatechange= function(){
+    if (this.readyState==XMLHttpRequest.DONE && this.status == 200) {
+        let respond = JSON.parse(this.responseText);
+        for (let index = 0; index < respond.length; index++) {
+            
+            if(respond[index].name=="Morocco")
+            {
+                $('#country').append('<option value="'+respond[index].name+'" selected>'+respond[index].name+'</option>');
+            }
+            else $('#country').append('<option value="'+respond[index].name+'">'+respond[index].name+'</option>');
+            
+        }
+    }
+    else if(this.readyState==XMLHttpRequest.DONE){
+        console.log("oups");
+    }
+}
+request_countries.open("GET","https://restcountries.eu/rest/v2/all?fields=name");
+request_countries.send();
